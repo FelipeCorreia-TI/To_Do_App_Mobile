72de8a497674ecb4f44a269a67d4bde0a2134258
Felipe Correia <felipecorreiatech@gmail.com>
2026-06-15 12:58:02 -0300

    Feature:MVP do app

--- app/src/main/java/com/example/todoapplocal/MainActivity.kt
+++ app/src/main/java/com/example/todoapplocal/MainActivity.kt
@@ -1,30 +1,15 @@
 package com.example.todoapplocal
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.todoapplocal.ui.theme.ToDoAppLocalTheme
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateListOf
 
 
 data class Tarefa (
@@ -34,7 +19,8 @@ data class Tarefa (
     var isConcluida: Boolean = false
 )
 
-val listadetarefa = remember { mutableStateListOf<Tarefa>() }
+
+val listadetarefa =  mutableStateListOf<Tarefa>()
 var proximoId = 1
 
 fun adicionarTarefa(texto:String,textoSecundario:String) {
@@ -44,44 +30,122 @@ fun adicionarTarefa(texto:String,textoSecundario:String) {
         proximoId++
     }
 }
+//Na função UPDATE atual, vai faltar a principal função de conseguir alterar as tarefas escritas.
+fun alternarStatusTarefa(id:Int){
+    val index = listadetarefa.indexOfFirst { it.id==id }
+    if (index != -1){
+        val tarefaAtual = listadetarefa[index]
 
-fun
+        listadetarefa[index] = tarefaAtual.copy(isConcluida = !tarefaAtual.isConcluida)
+    }
+}
+
+fun deletarTarefa(id: Int){
+    listadetarefa.removeIf { it.id == id  }
+}
 
 class MainActivity : androidx.activity.ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent{
-            TelaPrincipalTodo()
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ){
+                TeladotodoApp()
+            }
+
         }
 
     }
 }
 
 @Composable
-fun TelaPrincipalTodo() {
+
+fun TeladotodoApp(){
+    var textoDigitado by remember { mutableStateOf(" ")}
+    var descricaoDigitada by remember {mutableStateOf(" ")}
 
     Column(
-        modifier =
-            Modifier.
-            fillMaxSize().
-            padding(16.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            TextField(
-                value= textoTarefa,
-                onValueChange = { novoTexto ->
-                    textoTarefa = novoTexto
-                },
-                placeholder = {Text("Digite  uma nova tarefa...")},
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ){
+        Text(text = "Meu To-Do List Local", style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
+            OutlinedTextField(
+                value = textoDigitado,
+                onValueChange = { textoDigitado = it},
+                label = { Text("Nova tarefa...")},
                 modifier = Modifier.weight(1f)
             )
+
             Spacer(modifier = Modifier.width(8.dp))
 
+            OutlinedTextField(
+                value = descricaoDigitada,
+                onValueChange = { descricaoDigitada = it},
+                label = { Text("Descreva a tarefa...")},
+                modifier = Modifier.weight(1f)
+            )
 
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Button(onClick = {
+                adicionarTarefa(textoDigitado,descricaoDigitada)
+                textoDigitado = ""
+                descricaoDigitada = ""
+            }) {
+                Text("Add")
+            }
         }
+        Spacer(modifier = Modifier.width(16.dp))
 
+        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            items(listadetarefa.size){
+                    index -> val tarefa = listadetarefa[index]
+
+                ItemTarefa(
+                    tarefa = tarefa,
+                    onStatusItemChange = {
+                        alternarStatusTarefa(tarefa.id)},
+                    onDeletarClique = {deletarTarefa(tarefa.id)}
+                )
+            }
+        }
+    }
+}
+
+
+
+
+@Composable
+fun ItemTarefa(
+    tarefa: Tarefa,
+    onStatusItemChange: () -> Unit,
+    onDeletarClique:() -> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp).fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = tarefa.isConcluida,
+                        onCheckedChange =  { _ -> onStatusItemChange() }
+            )
+
+            Text(
+                text = tarefa.descricao,
+                modifier= Modifier.weight(1f).padding(start = 8.dp)
+            )
+
+            Button(onClick = onDeletarClique) {
+                Text("Excluir")
+            }
+        }
     }
 }
