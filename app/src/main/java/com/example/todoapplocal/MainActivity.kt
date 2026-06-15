ca1837abeb19a264ff6d026eee1c70dab771a31f
Felipe Correia <felipecorreiatech@gmail.com>
2026-06-15 09:43:30 -0300

    Feature:adiciona campo para texto e função para CREATE (CRUD)

--- app/src/main/java/com/example/todoapplocal/MainActivity.kt
+++ app/src/main/java/com/example/todoapplocal/MainActivity.kt
@@ -1,47 +1,87 @@
 package com.example.todoapplocal
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todoapplocal.ui.theme.ToDoAppLocalTheme
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateListOf
 
-class MainActivity : ComponentActivity() {
+
+data class Tarefa (
+    val id: Int,
+    val titulo: String,
+    val descricao: String,
+    var isConcluida: Boolean = false
+)
+
+val listadetarefa = remember { mutableStateListOf<Tarefa>() }
+var proximoId = 1
+
+fun adicionarTarefa(texto:String,textoSecundario:String) {
+    if(texto.isNotBlank()){
+        val nova = Tarefa(id = proximoId, titulo = texto, descricao = textoSecundario)
+        listadetarefa.add(nova)
+        proximoId++
+    }
+}
+
+fun
+
+class MainActivity : androidx.activity.ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ToDoAppLocalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+        setContent{
+            TelaPrincipalTodo()
         }
+
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TelaPrincipalTodo() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDoAppLocalTheme {
-        Greeting("Android")
+    Column(
+        modifier =
+            Modifier.
+            fillMaxSize().
+            padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            TextField(
+                value= textoTarefa,
+                onValueChange = { novoTexto ->
+                    textoTarefa = novoTexto
+                },
+                placeholder = {Text("Digite  uma nova tarefa...")},
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+
+        }
+
     }
 }
