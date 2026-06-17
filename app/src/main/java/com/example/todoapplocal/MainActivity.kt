101026e05d9b4c9269b1f3a5562af21180e26d6f
Felipe Correia <felipecorreiatech@gmail.com>
2026-06-17 10:10:47 -0300

    Feature:Adiciona Interatividade com o app: Sistema de 'sanfona', edição de tarefas, Check em tarefas.

--- app/src/main/java/com/example/todoapplocal/MainActivity.kt
+++ app/src/main/java/com/example/todoapplocal/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.todoapplocal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextDecoration
 
 
 data class Tarefa (
@@ -23,6 +27,7 @@ data class Tarefa (
 val listadetarefa =  mutableStateListOf<Tarefa>()
 var proximoId = 1
 
+//Create Local, Pega os 2 valores, coloca em uma variável e adiciona para a lista, gerando automaticamente um novo ID a cada CREATE
 fun adicionarTarefa(texto:String,textoSecundario:String) {
     if(texto.isNotBlank()){
         val nova = Tarefa(id = proximoId, titulo = texto, descricao = textoSecundario)
@@ -30,7 +35,9 @@ fun adicionarTarefa(texto:String,textoSecundario:String) {
         proximoId++
     }
 }
-//Na função UPDATE atual, vai faltar a principal função de conseguir alterar as tarefas escritas.
+
+
+//Função do Check para as tarefas, ele garante que esteja selecionando o id do item que o botão foi checado.
 fun alternarStatusTarefa(id:Int){
     val index = listadetarefa.indexOfFirst { it.id==id }
     if (index != -1){
@@ -40,6 +47,16 @@ fun alternarStatusTarefa(id:Int){
     }
 }
 
+//Update
+fun editarConteudoTarefa(id: Int,novoTitulo:String,novaDescricao: String){
+    val index = listadetarefa.indexOfFirst { it.id == id }
+    if (index != -1) {
+        val tarefaAtual= listadetarefa[index]
+        listadetarefa[index]= tarefaAtual.copy(titulo = novoTitulo, descricao = novaDescricao)
+    }
+}
+
+//DELETE de tarefas
 fun deletarTarefa(id: Int){
     listadetarefa.removeIf { it.id == id  }
 }
@@ -86,7 +103,7 @@ fun TeladotodoApp(){
             OutlinedTextField(
                 value = descricaoDigitada,
                 onValueChange = { descricaoDigitada = it},
-                label = { Text("Descreva a tarefa...")},
+                label = { Text("Descreva a tarefa(opcional)...")},
                 modifier = Modifier.weight(1f)
             )
 
@@ -104,13 +121,13 @@ fun TeladotodoApp(){
 
         LazyColumn(modifier = Modifier.fillMaxWidth()) {
             items(listadetarefa.size){
-                    index -> val tarefa = listadetarefa[index]
+                    index -> val Listatarefa = listadetarefa[index]
 
                 ItemTarefa(
-                    tarefa = tarefa,
-                    onStatusItemChange = {
-                        alternarStatusTarefa(tarefa.id)},
-                    onDeletarClique = {deletarTarefa(tarefa.id)}
+                    tarefa = Listatarefa,
+                    onStatusItemChange = { alternarStatusTarefa(Listatarefa.id)},
+                    onDeletarClique = {deletarTarefa(Listatarefa.id)},
+                    onSalvarEdicao = {novoT,novaD -> editarConteudoTarefa(Listatarefa.id,novoT,novaD)}
                 )
             }
         }
@@ -124,28 +141,97 @@ fun TeladotodoApp(){
 fun ItemTarefa(
     tarefa: Tarefa,
     onStatusItemChange: () -> Unit,
-    onDeletarClique:() -> Unit
+    onDeletarClique:() -> Unit,
+    onSalvarEdicao: (String,String) -> Unit
 ) {
+    var expandido by remember {mutableStateOf(false)}
+    var emEdicao by remember {mutableStateOf(false)}
+
+    var textoTituloEdicao by remember {mutableStateOf(tarefa.titulo)}
+    var textoDescricaoEdicao by remember {mutableStateOf(tarefa.descricao)}
+
+    LaunchedEffect(tarefa) {
+        textoTituloEdicao= tarefa.titulo
+        textoDescricaoEdicao = tarefa.descricao
+    }
     Card(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp)
+            .clickable{expandido=!expandido}
     ) {
-        Row(
-            modifier = Modifier.padding(16.dp).fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Checkbox(
-                checked = tarefa.isConcluida,
-                        onCheckedChange =  { _ -> onStatusItemChange() }
-            )
+        Column(modifier=Modifier.padding(16.dp)) {
+            Row(
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = tarefa.isConcluida,
+                    onCheckedChange =  { _ -> onStatusItemChange() }
+                )
 
-            Text(
-                text = tarefa.descricao,
-                modifier= Modifier.weight(1f).padding(start = 8.dp)
-            )
+                if(emEdicao){
+                    OutlinedTextField(
+                        value = textoTituloEdicao,
+                        onValueChange = {textoTituloEdicao=it},
+                        modifier =
+                            Modifier.weight(1f).padding(horizontal = 8.dp),
+                        label = {Text("Editar Título")}
+                    )
+                } else{
+                Text(
+                    text = tarefa.titulo,
+                    modifier= Modifier.weight(1f).padding(start = 8.dp),
+                    textDecoration = if(tarefa.isConcluida) TextDecoration.LineThrough else TextDecoration.None
+                )
+            }
 
-            Button(onClick = onDeletarClique) {
-                Text("Excluir")
+                Button(onClick = onDeletarClique, colors = ButtonDefaults.buttonColors(containerColor= MaterialTheme.colorScheme.error)) {
+                    Text("Excluir")
+                }
+            }
+            if (expandido){
+                Spacer(Modifier.height(12.dp))
+                HorizontalDivider(color=MaterialTheme.colorScheme.outlineVariant)
+                Spacer(modifier=Modifier.height(12.dp))
+                if(emEdicao){
+                    OutlinedTextField(
+                        value = textoDescricaoEdicao,
+                        onValueChange = {textoDescricaoEdicao=it},
+                        modifier = Modifier.fillMaxWidth(),
+                        label = {Text("Editar Descrição")}
+                    )
+                    Spacer(Modifier.height(8.dp))
+
+                    Button(
+                        onClick = {
+                            onSalvarEdicao(textoTituloEdicao,textoDescricaoEdicao)
+                            emEdicao = false
+                        },
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text("Salvar")
+                    }
+                }else {
+                    val textoExibido = if(tarefa.descricao.isNotBlank())
+                        tarefa.descricao else "Descrição: (Vazio)"
+                    Text(
+                        text = textoExibido,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Spacer(modifier = Modifier.align(Alignment.End))
+
+                    Button(
+                        onClick = {emEdicao=true},
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text("Editar")
+                    }
+                }
             }
         }
+
     }
 }
