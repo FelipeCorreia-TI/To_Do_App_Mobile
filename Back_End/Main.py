ed86f5aac67fb472f5c296082773f11a6affeaf6
Felipe Correia <felipecorreiatech@gmail.com>
2026-06-17 11:41:13 -0300

    Feature:Criação rotas ligação DB + Back_End rotas POST e GET

--- Back_End/Main.py
+++ Back_End/Main.py
@@ -1,14 +1,19 @@
+import urllib.parse
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Felipe:10072008FCSj#@127.0.0.1:3306/to_do_list'
+senha_original ='10072008FCSj#@'
+
+senha_aceita= urllib.parse.quote_plus(senha_original)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://Felipe:{senha_aceita}@127.0.0.1:3306/to_do_list'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Tarefa (db.Model):
+class Tarefas (db.Model):
     id= db.Column(db.Integer,primary_key=True)
     tarefa = db.Column(db.String(100))
     descricao = db.Column(db.Text)
@@ -17,14 +22,16 @@ class Tarefa (db.Model):
 
 @app.route('/tarefas', methods=['GET'])
 def lista():
-    tarefas = Tarefa.query.all()
+    tarefas = Tarefas.query.all()
     lista =[{"id":t.id,"tarefa":t.tarefa,"descricao":t.descricao,"checado":t.checado} for t in tarefas]
     return jsonify(lista)
 
 @app.route('/tarefas',methods=['POST'])
 def criar():
     dados = request.get_json()
-    nova = Tarefa(tarefa=dados['tarefa'],descricao=dados['descricao'])
+    nova = Tarefas(tarefa_post=dados['tarefa'],descricao_post=dados['descricao'])
+    db.session.add(nova)
+    db.session.commit()
     return jsonify({"status":"Sucesso"}),201
 
 if __name__ == '__main__':
