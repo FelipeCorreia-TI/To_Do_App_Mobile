625af70a2a8d5f8d6257ba4c49a08c9f936ea3db
Felipe Correia <felipecorreiatech@gmail.com>
2026-06-17 10:45:35 -0300

    mudança verdadeira no sql (anterior estava errada)

--- Data_Base/scriptdb.sql
+++ Data_Base/scriptdb.sql
@@ -2,7 +2,10 @@ create database to_do_list;
 use to_do_list;
 
 create table tarefas (
-	id int auto_increment primary key,
+    id int auto_increment primary key,
     tarefa varchar(500) not null,
-    descricao TEXT
+    descricao text,
+    checado char not null default 'N',
+
+    constraint chk_checado check(checado in ('S','N'))
 );
