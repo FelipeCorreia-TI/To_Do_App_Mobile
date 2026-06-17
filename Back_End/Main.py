bd3c3e63359d24476b0a391b56d9896e2699a15b
Felipe Correia <felipecorreiatech@gmail.com>
2026-06-17 12:52:58 -0300

    Feature+Fix:Adiciona Rotas e Arruma inconsistencia
    Adiciona:
    rota PUT e DELETE
    
    Arruma inconsistência de nomes, no database e back-end

--- Back_End/Main.py
+++ Back_End/Main.py
@@ -13,26 +13,54 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://Felipe:{senha_aceita}@
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Boa prática que ajuda a liberar recurso desncessário durante as requisições do banco
 db = SQLAlchemy(app)
 
-class Tarefas (db.Model): #Definição da tabela Tarefas no Server Side para garantir a conformidade dos dados
+class tarefas (db.Model): #Definição da tabela Tarefas no Server Side para garantir a conformidade dos dados
     id= db.Column(db.Integer,primary_key=True)
-    tarefa = db.Column(db.String(100))
+    titulo = db.Column(db.String(100))
     descricao = db.Column(db.Text)
     checado = db.Column(db.String(1),default='N') 
 
 
 @app.route('/tarefas', methods=['GET'])  #Rota GET - [Pegar dados]
 def lista():
-    tarefas = Tarefas.query.all() #ORM facilitando com uma query 'automática'
-    lista =[{"id":t.id,"tarefa":t.tarefa,"descricao":t.descricao,"checado":t.checado} for t in tarefas] #Percorre todos as colunas da tabela e atribui cada uma a um registro único 
+    tarefa = tarefas.query.all() #ORM facilitando com uma query 'automática'
+    lista =[{"id":t.id,"titulo":t.titulo,"descricao":t.descricao,"checado":t.checado} for t in tarefa] #Percorre todos as colunas da tabela e atribui cada uma a um registro único 
     return jsonify(lista) #Torna-se JSON para facilitar a comunicação Server <-> Back <-> Front
 
 @app.route('/tarefas',methods=['POST']) #Rota POST - [Para 'postar' os novos registros]
 def criar():
+
+    dados = request.get_json(silent=True) 
+     # Se 'dados' for nulo ou não for um dicionário, retorna um aviso amigável
+    if not dados or not isinstance(dados, dict):
+        return jsonify({"erro": "O corpo da requisição precisa ser um JSON válido e conter o cabeçalho 'Content-Type: application/json'"}), 400
+    
     dados = request.get_json() #Requisita o JSON para ter a estrutura e colocar os novos registros nas colunas sem que haja incoerências
-    nova = Tarefas(tarefa_post=dados['tarefa'],descricao_post=dados['descricao']) #Variável que através da DataClass lá de cima adiciona os novos registros dentro de variáveis que irão adicionar algum registro.
+    nova = tarefas(titulo=dados['titulo'],descricao=dados['descricao']) #Variável que através da DataClass lá de cima adiciona os novos registros dentro de variáveis que irão adicionar algum registro.
     db.session.add(nova) #faz o INSERT
     db.session.commit() #Commita a alteração
     return jsonify({"status":"Sucesso"}),201 #Retornará um status positivo, significa que foram adicionados | 200 - bateu e voltou as respostas | 201 - bateu e adicionou | 4xx - Erro |
+
+@app.route('/tarefas/<int:id>', methods=['PUT'])
+def atualizar(id):
+    dados = request.get_json()
+    tarefa= tarefas.query.get(id)
+
+    if not tarefa:
+        return jsonify({"erro": "Tarefa não encontrada"}), 404
+
+    tarefa.titulo = dados.get('titulo', tarefa.titulo)
+    tarefa.descricao = dados.get('descricao', tarefa.descricao)
+    tarefa.checado = dados.get('checado',tarefa.checado)
+
+    db.session.commit()
+    return jsonify({"status":"atualizado"})
+
+@app.route('/tarefas/<int:id>',methods=['DELETE'])
+def deletar(id):
+    tarefa = tarefas.query.get(id)
+    db.session.delete(tarefa)
+    db.sessions.commit()
+    return jsonify({"status":"Removido"})
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
