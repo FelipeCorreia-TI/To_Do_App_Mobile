061dede4e8076c4fe7f8026d583ef52e5efaf653
Felipe Correia <felipecorreiatech@gmail.com>
2026-06-19 13:42:35 -0300

    Fix: Arruma problema do grandle dos ultimos 3 commits.
    This reverts commit fc86c7b8bc8803d93d2c61201427edfc8b503eab.

--- Front_End/app/src/main/java/com/example/todoapplocal/MainActivity.kt
+++ Front_End/app/src/main/java/com/example/todoapplocal/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.todoapplocal
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,56 +9,50 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.style.TextDecoration
-
+import com.google.gson.annotations.SerializedName
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.launch
 
 data class Tarefa (
-    val id: Int,
-    val titulo: String,
-    val descricao: String,
-    var isConcluida: Boolean = false
+    @SerializedName("id") val id: Int = 0,
+    @SerializedName("titulo") val titulo: String,
+    @SerializedName("descricao") val descricao: String,
+    @SerializedName("concluido") val concluida: Boolean = false
 )
 
+interface ApiService{
+    @GET("/tarefas")
+    suspend fun buscarTarefas(): List<Tarefa>
 
-val listadetarefa =  mutableStateListOf<Tarefa>()
-var proximoId = 1
+    @POST("/tarefas")
+    suspend fun criarTarefa(@Body tarefa:Tarefa):
+            Response<Void>
 
-//Create Local, Pega os 2 valores, coloca em uma variável e adiciona para a lista, gerando automaticamente um novo ID a cada CREATE
-fun adicionarTarefa(texto:String,textoSecundario:String) {
-    if(texto.isNotBlank()){
-        val nova = Tarefa(id = proximoId, titulo = texto, descricao = textoSecundario)
-        listadetarefa.add(nova)
-        proximoId++
+    @PUT("/tarefas/{id}")
+    suspend fun atualizarTarefa(@Path("id") id:Int,@Body tarefa:Tarefa): Response<Void>
+
+    @DELETE("/tarefas/{id}")
+    suspend fun deletarTarefa(@Path("id") id:Int):Response<Void>
+}
+
+object RetrofitClient{
+    private const val BASE_URL = "http://10.0.2.2:5000"
+
+    val api: ApiService by lazy{
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
     }
 }
-
-
-//Função do Check para as tarefas, ele garante que esteja selecionando o id do item que o botão foi checado.
-fun alternarStatusTarefa(id:Int){
-    val index = listadetarefa.indexOfFirst { it.id==id }
-    if (index != -1){
-        val tarefaAtual = listadetarefa[index]
-
-        listadetarefa[index] = tarefaAtual.copy(isConcluida = !tarefaAtual.isConcluida)
-    }
-}
-
-//Update
-fun editarConteudoTarefa(id: Int,novoTitulo:String,novaDescricao: String){
-    val index = listadetarefa.indexOfFirst { it.id == id }
-    if (index != -1) {
-        val tarefaAtual= listadetarefa[index]
-        listadetarefa[index]= tarefaAtual.copy(titulo = novoTitulo, descricao = novaDescricao)
-    }
-}
-
-//DELETE de tarefas
-fun deletarTarefa(id: Int){
-    listadetarefa.removeIf { it.id == id  }
-}
-
 class MainActivity : androidx.activity.ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,53 +73,130 @@ class MainActivity : androidx.activity.ComponentActivity() {
 @Composable
 
 fun TeladotodoApp(){
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+
+    val listaDeTarefas = remember {mutableStateListOf<Tarefa>()}
+
     var textoDigitado by remember { mutableStateOf(" ")}
     var descricaoDigitada by remember {mutableStateOf(" ")}
 
+
+    var campoTitulo by remember {mutableStateOf("")}
+    var campoDescricao by remember {mutableStateOf("")}
+    var carregando by remember {mutableStateOf(false)}
+
+
+    fun atualizarListaDoServidor(){
+        coroutineScope.launch{
+            carregando = true
+            try{
+                val tarefasDoBanco =
+                    RetrofitClient.api.buscarTarefas()
+                    listaDeTarefas.clear()
+
+                listaDeTarefas.addAll(tarefasDoBanco)
+            } catch (e:Exception){
+                Toast.makeText(context,"Erro ao buscar dados: ${e.localizedMessage}",Toast.LENGTH_LONG).show()
+            } finally {
+                carregando = false
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        atualizarListaDoServidor()
+    }
+
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ){
-        Text(text = "Meu To-Do List Local", style = MaterialTheme.typography.headlineMedium)
+        Text(text = "Meu To-Do List Full-Stack", style = MaterialTheme.typography.headlineMedium)
         Spacer(modifier = Modifier.height(16.dp))
 
-        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
-            OutlinedTextField(
-                value = textoDigitado,
-                onValueChange = { textoDigitado = it},
-                label = { Text("Nova tarefa...")},
-                modifier = Modifier.weight(1f)
+        Column(modifier = Modifier.fillMaxWidth()){
+            OutlinedTextField(value= campoTitulo,
+                onValueChange = {campoTitulo=it},
+                label = {Text("Título da tarefa...")},
+                modifier = Modifier.fillMaxWidth()
             )
-
             Spacer(modifier = Modifier.width(8.dp))
 
-            OutlinedTextField(
-                value = descricaoDigitada,
-                onValueChange = { descricaoDigitada = it},
-                label = { Text("Descreva a tarefa(opcional)...")},
-                modifier = Modifier.weight(1f)
-            )
+            Button(
+                enabled= !carregando,
+                onClick= {
+                    if (campoTitulo.isNotBlank()){
+                        coroutineScope.launch {
+                            try{
+                                val novaTarefa = Tarefa(titulo = campoTitulo, descricao = campoDescricao)
 
-            Spacer(modifier = Modifier.width(8.dp))
+                                RetrofitClient.api.criarTarefa(novaTarefa)
+                                campoTitulo = ""
+                                campoDescricao = ""
 
-            Button(onClick = {
-                adicionarTarefa(textoDigitado,descricaoDigitada)
-                textoDigitado = ""
-                descricaoDigitada = ""
-            }) {
+                                atualizarListaDoServidor()
+                            } catch (e: Exception) {
+                                Toast.makeText(context,"Erro ao salvar",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                }
+            ) {
                 Text("Add")
             }
         }
-        Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.height(24.dp))
+
+        if(carregando) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+        }
 
         LazyColumn(modifier = Modifier.fillMaxWidth()) {
-            items(listadetarefa.size){
-                    index -> val Listatarefa = listadetarefa[index]
+            items(listaDeTarefas.size){
+                index -> val itemTarefa = listaDeTarefas[index]
 
-                ItemTarefa(
-                    tarefa = Listatarefa,
-                    onStatusItemChange = { alternarStatusTarefa(Listatarefa.id)},
-                    onDeletarClique = {deletarTarefa(Listatarefa.id)},
-                    onSalvarEdicao = {novoT,novaD -> editarConteudoTarefa(Listatarefa.id,novoT,novaD)}
+                itemTarefa(
+                    tarefa = itemTarefa,
+                    onStatusItemChange= {
+                        coroutineScope.launch {
+                            try {
+                                val tarefaAtualizada = itemTarefa.copy(concluida = !itemTarefa.concluida)
+
+                                RetrofitClient.api.atualizarTarefa(itemTarefa.id,tarefaAtualizada)
+                                atualizarListaDoServidor()
+                            } catch (e: Exception){
+                                Toast.makeText(context, "Erro ao atualizar status",  Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    },
+                    onDeletarClique = {
+                        coroutineScope.launch {
+                            try {
+                                RetrofitClient.api.deletarTarefa(itemTarefa.id)
+                                atualizarListaDoServidor()
+                            } catch (e: Exception){
+                                Toast.makeText(context,"Erro ao deletar",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    },
+                    onSalvarEdicao= { novoTitulo, novaDescricao ->
+                        coroutineScope.launch {
+                            try {
+                                val tarefaEditada =
+                                    itemTarefa.copy(titulo = novoTitulo, descricao = novaDescricao)
+
+                                RetrofitClient.api.atualizarTarefa(itemTarefa.id, tarefaEditada)
+
+                                atualizarListaDoServidor()
+                            } catch (e: Exception) {
+                                Toast.makeText(context, "Erro ao salvar edição", Toast.LENGTH_SHORT)
+                                    .show()
+                              }
+                         }
+                    }
                 )
             }
         }
@@ -135,14 +204,12 @@ fun TeladotodoApp(){
 }
 
 
-
-
 @Composable
 fun ItemTarefa(
     tarefa: Tarefa,
     onStatusItemChange: () -> Unit,
     onDeletarClique:() -> Unit,
-    onSalvarEdicao: (String,String) -> Unit
+    onSalvarEdicao: (String, String) -> Unit
 ) {
     var expandido by remember {mutableStateOf(false)}
     var emEdicao by remember {mutableStateOf(false)}
@@ -150,24 +217,23 @@ fun ItemTarefa(
     var textoTituloEdicao by remember {mutableStateOf(tarefa.titulo)}
     var textoDescricaoEdicao by remember {mutableStateOf(tarefa.descricao)}
 
-    LaunchedEffect(tarefa) {
-        textoTituloEdicao= tarefa.titulo
-        textoDescricaoEdicao = tarefa.descricao
-    }
+
     Card(
 
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 6.dp)
-            .clickable{expandido=!expandido}
+            .clickable { expandido = !expandido }
     ) {
         Column(modifier=Modifier.padding(16.dp)) {
             Row(
-                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Checkbox(
-                    checked = tarefa.isConcluida,
+                    checked = tarefa.concluido,
                     onCheckedChange =  { _ -> onStatusItemChange() }
                 )
 
@@ -176,14 +242,18 @@ fun ItemTarefa(
                         value = textoTituloEdicao,
                         onValueChange = {textoTituloEdicao=it},
                         modifier =
-                            Modifier.weight(1f).padding(horizontal = 8.dp),
+                            Modifier
+                                .weight(1f)
+                                .padding(horizontal = 8.dp),
                         label = {Text("Editar Título")}
                     )
                 } else{
                 Text(
                     text = tarefa.titulo,
-                    modifier= Modifier.weight(1f).padding(start = 8.dp),
-                    textDecoration = if(tarefa.isConcluida) TextDecoration.LineThrough else TextDecoration.None
+                    modifier= Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp),
+                    textDecoration = if(tarefa.concluida) TextDecoration.LineThrough else TextDecoration.None
                 )
             }
 
