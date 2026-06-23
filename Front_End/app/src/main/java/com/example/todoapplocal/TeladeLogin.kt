442634859d1f087625fa3a3836dfce31132b9f4a
Felipe Correia <felipecorreiatech@gmail.com>
2026-06-23 13:18:59 -0300

    Feature:Adiciona Tela de Login com back-end

--- Front_End/app/src/main/java/com/example/todoapplocal/TeladeLogin.kt
+++ Front_End/app/src/main/java/com/example/todoapplocal/TeladeLogin.kt
@@ -1,20 +1,26 @@
 package com.example.todoapplocal
 
-import android.graphics.Outline
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun TelaLogin(onLoginSucesso:() -> Unit ){
     var email by remember { mutableStateOf("") }
     var senha by remember {mutableStateOf("")}
     var erroMensagem by remember { mutableStateOf("") }
+
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
+    var carregando by remember { mutableStateOf(false)}
 
     Column(
         modifier= Modifier
@@ -23,7 +29,7 @@ fun TelaLogin(onLoginSucesso:() -> Unit ){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Text(text= "Bem-vindo ao seu TO-DO-LIST", style = MaterialTheme.typography.headlineLarge)
+        Text(text= "Bem-vindo ao seu TO-DO-LIST APP", style = MaterialTheme.typography.headlineLarge)
 
         Spacer(modifier=Modifier.height(24.dp))
 
@@ -51,16 +57,70 @@ fun TelaLogin(onLoginSucesso:() -> Unit ){
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        Button(onClick ={
-            if(email == "felipe@teste.com" && senha == "123456"){
-                onLoginSucesso()
-            }else {
-                erroMensagem = "E-mail ou senha incorretos!"
+        Button(enabled =!carregando,
+            onClick = {
+            if(email.isNotBlank() && senha.isNotBlank()){
+                coroutineScope.launch {
+                    carregando = true
+                    erroMensagem = ""
+                    try {
+                        val resposta =
+                            RetrofitClient.api.efetuarLogin(LoginRequest(email,senha))
+                        if(resposta.isSuccessful && resposta.body() != null){
+                            RetrofitClient.usuarioLogadoId=
+                                resposta.body()!!.usuarioId
+                                Toast.makeText(context,"Bem-Vindo!", Toast.LENGTH_SHORT).show()
+                                onLoginSucesso()
+                        } else{
+                            erroMensagem = "E-mail ou senha incorretos!"
+                        }
+                    } catch (e: Exception){
+                        erroMensagem = "Erro de conexão com o servidor."
+                    } finally {
+                        carregando = false
+                    }
+                }
+            } else {
+                erroMensagem = "Preencha todos os campos!"
             }
         },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Entrar")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedButton(
+            enabled = !carregando,
+            onClick = {
+                if (email.isNotBlank() && senha.isNotBlank()){
+                    if(senha.length < 6){
+                        erroMensagem = "A senha deve ter no mínimo 6 caracteres!"
+                        return@OutlinedButton
+                    }
+                    coroutineScope.launch {
+                        carregando = true
+                        erroMensagem = ""
+                        try{
+                            val resposta = RetrofitClient.api.cadastrarUsuario(LoginRequest(email,senha))
+                            if(resposta.isSuccessful){
+                                Toast.makeText(context,"Conta criada! Clique em Entrar.", Toast.LENGTH_LONG).show()
+                                senha = ""
+                            }else{
+                                erroMensagem = "Este e-mail já está cadastrado."
+                            }
+                        } catch (e: Exception){
+                            erroMensagem = "Erro ao tentar cadastrar."
+                        } finally {
+                            carregando = false
+                        }
+                    }
+                } else {
+                    erroMensagem = "Preencha e-mail e senha para cadastrar !" }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Criar Nova conta")
         }
     }
 }
